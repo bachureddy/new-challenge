cd38166205b960e6fa2e37e4b2e374dd8a76cc49
bachureddy <152122226+bachureddy@users.noreply.github.com>
2026-09-05 21:26:56 -0700

    Change age check from 18 to 20 for adulthood

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Checking a condition to decide which block of code to run
 age = int(input("Enter your age: "))
 
-if age >= 18:
+if age >= 20:
     print("You are an adult.")
 else:
     print("You are a minor.")
